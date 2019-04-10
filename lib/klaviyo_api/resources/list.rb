168d541b3294dd31aa2c1919412c82e94c8ddc92
Marc Morinville <marc@rewind.io>
2019-04-10 09:01:49 -0400

    Add enumerator to get all Members of a List

--- lib/klaviyo_api/resources/list.rb
+++ lib/klaviyo_api/resources/list.rb
@@ -12,5 +12,29 @@ module KlaviyoAPI
         super.tap { |record| record.id = scope }
       end
     end
+
+    # Gets all Members of this List. Uses the Groups endpoint.
+    #
+    # https://www.klaviyo.com/docs/api/v2/lists#get-members-all
+    def members(options = {})
+      CachingEnumerator.new do |yielder|
+        marker = nil
+
+        loop do
+          path = "#{self.class.prefix}group/#{id}/members/all"
+          path += "?marker=#{marker}" if marker
+
+          response = JSON.parse(connection.get(path, self.class.headers).body)
+          marker = response['marker']
+          list_members = response['records']
+
+          list_members.each do |list_member|
+            yielder.yield KlaviyoAPI::ListMember.new list_member
+          end
+
+          break if marker.nil?
+        end
+      end
+    end
   end
 end
