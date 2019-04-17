a31e97f5d37cb149f78496104b52a57c0ecfa11c
Marc Morinville <marc@rewind.io>
2019-04-17 14:39:20 -0400

    Improve .count performance by specifying undocumented count parameter

--- test/resources/support/test_countable.rb
+++ test/resources/support/test_countable.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 describe KlaviyoAPI::Support::Countable do
   before do
-    stub_request(:get, %r{https://a.klaviyo.com/api/v1/people})
+    stub_request(:get, "https://a.klaviyo.com/api/v1/people?api_key=&count=1")
       .to_return body: load_fixture(:people)
   end
 
