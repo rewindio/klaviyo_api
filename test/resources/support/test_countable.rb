aca6bc3972b9ab53fb0984ba1a687fed4cf76ab3
Marc Morinville <marc@rewind.io>
2019-04-18 08:33:10 -0400

    Add new fixture for count test

--- test/resources/support/test_countable.rb
+++ test/resources/support/test_countable.rb
@@ -5,10 +5,10 @@ require 'test_helper'
 describe KlaviyoAPI::Support::Countable do
   before do
     stub_request(:get, "https://a.klaviyo.com/api/v1/people?api_key=&count=1")
-      .to_return body: load_fixture(:people)
+      .to_return body: load_fixture(:count)
   end
 
   it 'successfully gets a count' do
-    assert_equal 3, KlaviyoAPI::Profile.count
+    assert_equal 567, KlaviyoAPI::Profile.count
   end
 end
