0681e40e129d9fff61ca09e706d6cbd5d72e7007
sanket <sanket@sankets-mbp.lan>
2019-09-12 12:28:59 -0400

    Add error checking incase there are no profiles in klaviyo

--- lib/klaviyo_api/resources/support/countable.rb
+++ lib/klaviyo_api/resources/support/countable.rb
@@ -5,6 +5,11 @@ module KlaviyoAPI::Support
     def count(params = {})
       all_params = params.deep_merge(params: { count: 1 })
       all(all_params).total
+    # klaviyo returns 400 if there are no people/profile in the account, so we catch it and display zero as the total subscribers
+    rescue => e
+      Nucleus::Logger.warn 'Received a 400 error, returning zero', error: e
+
+      0
     end
   end
 end
