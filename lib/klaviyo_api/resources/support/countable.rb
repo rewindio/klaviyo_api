5f35fc095087174ae5ff29f0202cc199c9aae651
sanket <sanket@sankets-mbp.lan>
2019-09-12 12:58:41 -0400

    Update Comment

--- lib/klaviyo_api/resources/support/countable.rb
+++ lib/klaviyo_api/resources/support/countable.rb
@@ -5,7 +5,7 @@ module KlaviyoAPI::Support
     def count(params = {})
       all_params = params.deep_merge(params: { count: 1 })
       all(all_params).total
-    # klaviyo returns 400 if there are no people/profile in the account, so we catch it and display zero as the total subscribers
+    # klaviyo returns 400 if there are no results found in the account, so we catch it and return zero
     rescue ActiveResource::BadRequest => e
       Nucleus::Logger.warn 'Received a 400 error, returning zero', error: e
 
