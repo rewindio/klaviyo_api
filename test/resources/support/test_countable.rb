69e56c8059aab2a5009b7c9910a932ecdb9c2cd7
sanket <sanket@sankets-mbp.lan>
2019-09-12 14:46:07 -0400

    Update tests

--- test/resources/support/test_countable.rb
+++ test/resources/support/test_countable.rb
@@ -3,12 +3,13 @@
 require 'test_helper'
 
 describe KlaviyoAPI::Support::Countable do
-  before do
-    stub_request(:get, "https://a.klaviyo.com/api/v1/people?api_key=&count=1")
-      .to_return body: load_fixture(:paginated_collection)
+  it 'successfully gets a count' do
+    stub_request(:get, 'https://a.klaviyo.com/api/v1/people?api_key=&count=1').to_return body: load_fixture(:paginated_collection)
+    assert_equal 567, KlaviyoAPI::Profile.count
   end
 
-  it 'successfully gets a count' do
-    assert_equal 567, KlaviyoAPI::Profile.count
+  it 'returns zero when 400 error is returned from klaviyo' do
+    stub_request(:get, 'https://a.klaviyo.com/api/v1/people?api_key=&count=1').to_return(status: 400, body: '')
+    assert_equal 0, KlaviyoAPI::Profile.count
   end
 end
